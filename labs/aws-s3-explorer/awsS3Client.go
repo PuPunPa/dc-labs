bb7f35292721bb2fa362318104b34346b6d37085
Jose Pablo Lopez Portillo <0211604@up.edu.mx>
2021-03-17 05:46:44 +0000

    Solve aws-s3-explorer
    Signed-off-by: Jose Pablo Lopez Portillo <0211604@up.edu.mx>

--- labs/aws-s3-explorer/awsS3Client.go
+++ labs/aws-s3-explorer/awsS3Client.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strconv"
+)
+
+func handleDial(port int, bucket string, c chan string) {
+	conn, err := net.Dial("tcp", "localhost:"+strconv.Itoa(port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	_, err = io.WriteString(conn, bucket)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bs := bufio.NewReader(conn)
+	line, _, _ := bs.ReadLine()
+	c <- string(line)
+}
+
 func main() {
+	portPtr := flag.Int("proxy", 8000, "proxy to dial")
+	bucketPtr := flag.String("bucket", "ryft-public-sample-data", "bucket to request")
+	flag.Parse()
+	c := make(chan string, 1)
+	go handleDial(*portPtr, *bucketPtr, c)
+	for item := range c {
+		fmt.Println(item)
+	}
+	close(c)
 }
--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -1,4 +1,106 @@
 package main
 
+import (
+	"bufio"
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type ListBucketResult struct {
+	Contents []Contents `xml:"Contents`
+}
+
+type Contents struct {
+	Key string `xml:"Key"`
+}
+
+type Send struct {
+	BucketName       string
+	ObjectsCount     int
+	DirectoriesCount int
+	Extensions       map[string]int
+}
+
+func writeJson(bucket string) []byte {
+	resp, err := http.Get("http://s3.amazonaws.com/" + bucket)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var body ListBucketResult
+	xml.Unmarshal(bodyBytes, &body)
+
+	content, err := json.Marshal(body.Contents)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contentStr := string(content[1 : len(content)-1])
+	keys := strings.Split(contentStr, ",")
+
+	objsCnt := len(keys)
+	dirs := make(map[string]int)
+	exts := make(map[string]int)
+
+	for i := 0; i < objsCnt; i++ {
+		key := string(keys[i][8 : len(keys[i])-2])
+		dir := strings.Split(key, "/")
+		if len(dir) > 1 {
+			dirs[dir[0]] = 1
+		}
+		ext := strings.Split(key, ".")
+		if len(ext) > 1 {
+			exts[ext[len(ext)-1]] += 1
+		}
+	}
+	s := Send{bucket, objsCnt, len(dirs), exts}
+	send, err := json.Marshal(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return send
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		bs := bufio.NewReader(c)
+		bucket, _, _ := bs.ReadLine()
+		_, err := io.WriteString(c, string(writeJson(string(bucket))))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
+	portPtr := flag.Int("port", 9090, "port to be used")
+	flag.Parse()
+	portStr := "localhost:" + strconv.Itoa(*portPtr)
+	listener, err := net.Listen("tcp", portStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn) // handle connections concurrently
+	}
 }
